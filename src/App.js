2d134a4c698b607698e328b39955ad055c715804
cusmoritz <cus.moritz@gmail.com>
2023-08-14 15:50:53 -0600

    feat: boilerplate files, adding menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { Home } from './Home';
+import { Blog } from './Blog';
+import { Contact } from './Contact';
+import { Projects } from './Projects';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+        Welcome
+      </header> */}
+      <div className='window-container'>
+        <div className='title-bar'>
+          <div className='title-text'>www.marcus.me</div>
+          <button className='help-button'>?</button>
+        </div>
+        
+          <menu role="tablist">
+            <li role="tab" selected="true" label="Home"><a href="/">Home</a></li>
+            <li role="tab" selected="false" label="Blog"><a href="/blog">Blog</a></li>
+            <li role="tab" selected="false" label="Projects"><a href="/projects">Projects</a></li>
+            <li role="tab" selected="false" label="Contact"><a href="/contact">Contact</a></li>
+          </menu>
+          
+        </div>
+      
+      {/* <ul>
+        <li><Home /></li>
+        <li><Blog /></li>
+        <li> <Contact /></li>
+        <li><Projects /></li>
+      </ul> */}
+
     </div>
   );
 }
