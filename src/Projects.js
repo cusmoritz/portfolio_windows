73026c62c6c590c62f53540bc72497c3673d56b0
cusmoritz <cus.moritz@gmail.com>
2023-08-15 18:02:09 -0600

    feat: added react-router, filled in some other pages with 'content'

--- src/Projects.js
+++ src/Projects.js
@@ -3,7 +3,56 @@ import React from "react";
 export const Projects = () => {
     return (
         <>
-        <h2>Projects</h2>
+            <menu role="tablist">
+            <li 
+                role="tab" 
+                selected="true" 
+                aria-selected="false" 
+                label="Home">
+                    <a href="/">Home</a>
+            </li>
+            <li 
+                role="tab" 
+                selected="false" 
+                aria-selected="false" 
+                label="Blog">
+                    <a href="/blog">Musings</a>
+            </li>
+            <li 
+                role="tab" 
+                selected="false" 
+                aria-selected="true" 
+                label="Projects">
+                    <a href="/projects">Projects</a>
+            </li>
+        </menu>
+
+        <div className='main-content-container' role="tabpanel">
+          <div className='main-content' >
+            <h2>This is where I would put a list of personal / professional projects or writings! 💾</h2>
+            <ul className='home-tree'>
+              <li>Musings</li>
+              <li>Work and Interests</li>
+                <ul>
+                  <li>Copy editor ✒️</li>
+                  <li>Software developer 💾</li>
+                  <li>News 🗞️</li>
+                  <li>Skiing ⛷️</li>
+                </ul>
+              <li>
+                <details closed>
+                  <summary>Links:</summary>
+                  <ul>
+                    <li>Github 🤖</li>
+                    <li>Email ✉️</li>
+                    <li>LinkedIn 🔗</li>
+                  </ul>
+                </details>
+              </li>
+            </ul>
+          </div>
+        </div>
+
         </>
     )
 }
